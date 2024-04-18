8f4a7936350f8f2c17332c088c6c98e7927e5b34
Muhammad Adeel Shakir <captainadeel000@gmail.com>
2024-04-18 11:40:36 +0500

    upload image in categories

---
+++ Grocery Store/admin/includes/sidebar.php
@@ -0,0 +1,43 @@
+        <!--**********************************
+            Sidebar start
+        ***********************************-->
+        <div class="nk-sidebar">
+            <div class="nk-nav-scroll">
+
+                <ul class="metismenu" id="menu">
+                    <li class="nav-label">Dashboard</li>
+                    <li>
+                        <a href="index.php" aria-expanded="false">
+                        <i class="fa-solid fa-gauge-high text-primary"></i> <span class="nav-text">Dashboard</span>
+                        </a>
+
+                    </li>
+
+
+                    <li>
+                        <a href="categories.php" aria-expanded="false">
+                        <i class="fa-solid fa-layer-group text-primary"></i> <span class="nav-text"> Categories</span>
+                        </a>
+                    </li>
+
+
+                    <li>
+                        <a href="products.php" aria-expanded="false">
+                        <i class="fa-solid fa-cubes text-primary"></i> <span class="nav-text"> Products</span>
+                        </a>
+                    </li>
+
+
+                    <li>
+                        <a href="#" aria-expanded="false">
+                        <i class="fa-solid fa-cart-shopping text-primary"></i> <span class="nav-text"> Orders</span>
+                        </a>
+                    </li>
+
+
+                </ul>
+            </div>
+        </div>
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
