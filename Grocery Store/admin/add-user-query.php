894705e59be4f3dd5f3205580dc70494b0448661
Muhammad Adeel Shakir <captainadeel000@gmail.com>
2024-04-24 21:19:18 +0500

    view-user page created

---
+++ Grocery Store/admin/add-user-query.php
@@ -0,0 +1,44 @@
+<?php
+
+// db connection
+
+require_once "./db-con.php";
+require_once "./includes/helpers.php";
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    // echo "<pre>"; print_r($_POST); echo "</pre>";
+    // echo "<pre>"; print_r($_FILES); echo "</pre>";
+
+        // upload image
+        $data = uploadImage("users", $_FILES['image']);
+
+        if ($data['errors'] === false) {
+            // save info into db
+            $name = $data['result'];
+
+            // echo $name;
+            // exit;
+
+            $query = "INSERT INTO `users`(`name`, `email`, `password`, `image`, `mobile`, `address`, `description`) 
+            VALUES ('$_POST[name]','$_POST[email]' ,'$_POST[password]','$_POST[image]',$_POST[mobile],'$_POST[address]', '$_POST[description]') ";
+    
+            if (mysqli_query($con, $query)) {
+                header("Location:view-user.php");
+            }
+            else{
+                echo "<div class='alert alert-danger mt-2 uploadingErr'>Query Failed</div>";
+            }
+        }
+        else{
+            
+          echo  "<div class='alert alert-danger mt-2 uploadingErr'> $data[result]</div>";
+
+        }
+
+    exit;
+}
+
+
+?>
--- Grocery Store/admin/add-user.php
+++ Grocery Store/admin/add-user.php
@@ -31,28 +31,28 @@
             <hr>
 
             <div class="d-flex justify-content-end">
-                <a href="products.php" class="btn btn-info text-white"><i class="fa fa-eye"></i> View User</a>
+                <a href="view-user.php" class="btn btn-info text-white"><i class="fa fa-eye"></i> View User</a>
             </div>
 
             <div class="form-container">
-                <form action="./product_query.php" method="POST" enctype="multipart/form-data" class="row">
+                <form action="./add-user-query.php" method="POST" enctype="multipart/form-data" class="row">
 
-                    <div class="col-lg-4 mb-2">
+                    <div class="col-lg-6 mb-2">
                         <label class="form-label" for="name">Full Name <span class="text-danger">*</span>
                         </label>
                         <input type="text" class="form-control" id="name" name="name" placeholder="Enter here..." required>
                     </div>
 
 
-                    <div class="col-lg-4 mb-2">
+                    <div class="col-lg-6 mb-2">
                         <label class="form-label" for="email">Email <span class="text-danger">*</span>
                         </label>
-                        <input type="number" class="form-control" id="email" name="email" placeholder="Enter here..." required>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter here..." required>
                     </div>
 
 
 
-                    <div class="col-lg-4 mb-2">
+                    <div class="col-lg-6 mb-2">
                         <label class="form-label" for="password">Password <span class="text-danger">*</span>
                         </label>
                         <input type="password" class="form-control" id="password" name="password" placeholder="Enter here..." required>
@@ -75,10 +75,16 @@
                     </div>
 
 
-                    <div class="col-lg-12 mb-2">
+                    <div class="col-lg-6 mb-2">
                         <label class="form-label" for="address">Address <span class="text-danger">*</span>
                         </label>
-                        <textarea name="address" class="form-control" id="" rows="5"></textarea>
+                       <input type="text" class="form-control" name="address" id="address" placeholder="Enter here..." required>
+                    </div>
+
+                    <div class="col-lg-12 mb-2">
+                        <label class="form-label" for="address">Description <span class="text-danger">*</span>
+                        </label>
+                       <input type="text" class="form-control" name="description" id="description" placeholder="Enter here..." required>
                     </div>
 
                     <div class="offset-8 col-lg-4 mb-2">
